b2801573bfcb8d77c44ee2dd8b734f87663a60a8
Peter Smith <petertonysmith94@gmail.com>
2023-07-15 14:53:12 +0100

    Added a basic socials component

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,3 +1,35 @@
-export const Socials = () => {
-  
+import styled from 'styled-components'
+
+export type Social = {
+  name: string,
+  url: string,
+  iconUrl: string
 }
+
+export const Socials = (socials: Array<Social>) => {
+  const SocialIcon = ({ url, iconUrl, name }: Social) => {
+    return (
+      <SocialLink href={url} target="_blank" rel="noreferrer">
+        <SocialImage src={iconUrl} alt={name.toLowerCase()} />
+      </SocialLink>
+    )
+  }
+
+  return (
+    <SocialsContainer>
+      { socials.map(social => <SocialIcon key={social.name} {...social} />) }
+    </SocialsContainer>
+  )
+}
+
+export const SocialsContainer = styled.div`
+  display: flex;
+`
+
+export const SocialLink = styled.a`
+  display: inline-block;
+`
+
+export const SocialImage = styled.img`
+
+`
