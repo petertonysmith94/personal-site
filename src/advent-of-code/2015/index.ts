f65d127b268209ff47e263f13bd733a0c977c865
Peter Smith <petertonysmith94@gmail.com>
2023-07-17 21:10:44 +0100

    Advent of code: 2015/2 - attempt 1

--- src/advent-of-code/2015/index.ts
+++ src/advent-of-code/2015/index.ts
@@ -1,9 +1,11 @@
 import { AdventYearConfig } from "../types"
 import { day1 } from "./1/config"
+import { day2 } from "./2/config"
 
 export const twentyFifteen: AdventYearConfig = {
   year: 2015,
   days: [
-    day1
+    day1,
+    day2
   ]
 }
