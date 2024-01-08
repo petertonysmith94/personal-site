d2150de59796b04c443d74891c51868f14f1c06d
Peter Smith <peter@blueoceancomputing.co.uk>
2024-01-08 16:48:50 +0000

    feat: Add custom webpack config and update package.json scripts

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/**
+ * Provides a way to add custom webpack config
+ * 
+ * @param {*} config webpack config
+ * @param {*} env the environment
+ */
+module.exports = function override(config, env) {
+
+  return config;
+}
