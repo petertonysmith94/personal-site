61b0c5811e17b0d8770fc0a9596e1c8372744d78
Peter Smith <peter@blueoceancomputing.co.uk>
2024-01-08 18:56:24 +0000

    feat: consolidate and simplify

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,26 @@
 import { ThemeProvider, useTheme } from "styled-components";
 import { GlobalReset, GlobalStyle } from "../components/global-style";
 import { createTheme } from "../theme/theme";
-import { Pages, MarkdownPage } from "../components/page";
 import { Socials } from "../components/socials";
 import { config } from "../config";
-import { HomeWrapper, HeroWrapper, Divider, SocialsWrapper } from "./app-content.styles";
+import { HomeWrapper, HeroWrapper, Divider, SocialsWrapper, H1, P, Pages, Page } from "./app-content.styles";
 import Profile from '../pages/profile.md'
 import Experience from '../pages/experience.md'
 import Projects from '../pages/projects.md'
 import Contact from '../pages/contact.md'
+import ReactMarkdown from "react-markdown";
+
+const MarkdownPage = ({ children }: { children: string }) => (
+  <Page>
+    <ReactMarkdown
+      children={children}
+      components={{
+        h1: ({ node, ...props }) => <H1 {...props} />,
+        p: ({ node, ...props }) => <P {...props} />,
+      }}
+    />
+  </Page>
+)
 
 const AppContent = () => {
   const theme = useTheme();
@@ -18,7 +30,11 @@ const AppContent = () => {
   return (
     <HomeWrapper>
       <HeroWrapper>
-        <Logo color={theme.accent} />
+        <Logo
+          width={250}
+          height={250}
+          color={theme.accent}
+        />
         <SocialsWrapper>
           <Socials
             socials={socials}
--- src/components/page/index.ts
+++
@@ -1,3 +0,0 @@
-export { MarkdownPage } from './markdown-page.component';
-export { Pages, Page } from './page.styles';
-
